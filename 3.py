93e77d678f9c11d95e24f0ef1750284c10226ce5
Sindy Tan <sindytn@gmail.com>
2024-08-09 17:34:00 -0700

    Add problem 3

---
+++ 3.py
@@ -0,0 +1,21 @@
+'''
+Approach: Consider the prime factorization of N. Sort and iteratively remove all powers 
+of prime factors. Since the factors were sorted, the last one removed is the largest. 
+When the last factor is removed, the quotient becomes one, so we can use this as our 
+stopping condition.
+'''
+
+N = 600851475143
+
+dividend = N
+divisor = 2
+result = 1  # The last divisor
+
+while result < dividend:
+    if dividend % divisor == 0:
+        result = divisor
+    while dividend % divisor == 0:
+        dividend /= divisor
+    divisor += 1
+
+print(f"{result=}")
